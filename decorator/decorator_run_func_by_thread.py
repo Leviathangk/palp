2b6bc08b7b2460ea41dd1ab2b2fb4cd43e4f9b12
Leviathan <1015295213@qq.com>
2022-12-13 20:25:18 +0800

    增加线程执行装饰器

---
+++ decorator/decorator_run_func_by_thread.py
@@ -0,0 +1,32 @@
+"""
+    通过线程执行函数，简化创建线程
+"""
+import threading
+from typing import Callable
+
+
+class RunByThread(threading.Thread):
+    """
+        通过线程执行函数，简化创建线程
+    """
+
+    def __init__(self, callback: Callable = None, **kwargs):
+        super().__init__(**kwargs)
+        self.func = None
+        self.args = None
+        self.kwargs = None
+        self.callback = callback
+
+    def run(self) -> None:
+        res = self.func(*self.args, *self.kwargs)
+        if self.callback:
+            self.callback(res)
+
+    def __call__(self, func: Callable):
+        def _wrapper(*args, **kwargs):
+            self.func = func
+            self.args = args
+            self.kwargs = kwargs
+            self.start()
+
+        return _wrapper
