b5f0cf108b822aaa99d9af7f5155834479633b9e
Leviathan <1015295213@qq.com>
2022-12-30 14:46:37 +0800

    新spider:jump spider

---
+++ controller/controller_spider_jump.py
@@ -0,0 +1,150 @@
+"""
+    jump spider 控制器
+"""
+import inspect
+from palp import settings
+from loguru import logger
+from typing import Union, List, Callable
+from palp.network.request import Request
+from palp.network.response import Response
+from palp.sequence.sequence import Sequence
+from palp.exception import DropRequestException
+
+
+class JumpController:
+    REQUEST_MIDDLEWARE = {}
+
+    def __init__(self, q: Sequence, request_middleware: Union[List[Callable], Callable], spider):
+        """
+
+        :param q: 请求队列
+        :param request_middleware: 请求中间件
+        :param spider: jump spider
+        """
+        # 导入设置
+        if request_middleware is None:
+            request_middleware = []
+        elif not isinstance(request_middleware, list):
+            request_middleware = [request_middleware]
+
+        # 实例化
+        for i, m in enumerate(request_middleware):
+            if m not in self.__class__.REQUEST_MIDDLEWARE:
+                request_middleware[i] = m()
+                self.__class__.REQUEST_MIDDLEWARE[m] = request_middleware[i]
+            else:
+                request_middleware[i] = self.__class__.REQUEST_MIDDLEWARE[m]
+
+        self.queue = q
+        self.spider = spider
+        self.request_middleware = request_middleware
+
+    def run(self):
+        """
+        执行流程
+
+        :return:
+        """
+        request = None
+
+        while True:
+            task = self.queue.get(timeout=0.5)
+
+            try:
+                if task is None:
+                    break
+                elif not isinstance(task, Request):
+                    continue
+
+                request = task
+                self.run_requests(request=task)
+            except DropRequestException as e:
+                logger.warning(f"丢弃请求：{e.args}")
+
+        if request:
+            self.spider.jump_out(request)
+
+    def add_new_request(self, new_request: Request, old_request: Request, response: Response):
+        """
+        添加新的请求到队列中
+
+        :param new_request:
+        :param old_request:
+        :param response:
+        :return:
+        """
+        # 自动衔接 meta
+        new_request.meta = old_request.meta
+
+        # 自动拼接 url
+        if not new_request.url.startswith('http') and response:
+            new_request.url = response.urljoin(new_request.url)
+
+        # 添加请求必须参数
+        new_request.cookie_jar = old_request.cookie_jar  # 续上上一个的 cookie_jar
+
+        # 修改优先级，深层的函数应该优先处理，避免积压不前（深度爬取）
+        if settings.REQUEST_QUEUE_MODE == 3:
+            new_request.priority = old_request.priority - 1
+
+        self.queue.put(new_request)
+
+    def run_requests(self, request: Request):
+        """
+        处理 yield 的 Request
+
+        :param request:
+        :return:
+        """
+        # 请求发起前的处理
+        for middleware in self.request_middleware:
+            middleware.request_in(self.spider, request)
+
+        # 处理请求
+        response = None
+        failed_times = 0
+        while failed_times < settings.REQUEST_RETRY_TIMES:
+            try:
+                response = request.send()
+                break
+            except DropRequestException as e:
+                raise DropRequestException(self.spider.name, request, *e.args)
+            except Exception as e:
+                failed_times += 1
+
+                # 出现错误可直接处理，并返回新的请求，同时阻断当前错误请求传播
+                for middleware in self.request_middleware:
+                    new_request = middleware.request_error(self.spider, request, e)
+                    if new_request is None:
+                        continue
+                    elif isinstance(new_request, Request):
+                        self.add_new_request(new_request=new_request, old_request=request, response=response)
+                        raise DropRequestException(middleware.__class__.__name__, str(request))
+                    else:
+                        logger.warning("request_error 仅支持 Request 返回值！")
+
+            # 失败达到最大次数直接丢弃
+            if failed_times >= settings.REQUEST_RETRY_TIMES:
+                for middleware in self.request_middleware:
+                    middleware.request_failed(self.spider, request)
+                return
+
+        # 请求结束的回调（可在此时判断返回值是否合理，不合理可在此重新构造请求，同时阻断当前请求传播）
+        for middleware in self.request_middleware:
+            new_request = middleware.request_close(self.spider, request, response)
+            if new_request is None:
+                continue
+            elif isinstance(new_request, Request):
+                self.add_new_request(new_request=new_request, old_request=request, response=response)
+                raise DropRequestException(middleware.__class__.__name__, str(request))
+            else:
+                logger.warning("request_close 仅支持 Request 返回值！")
+
+        # 继续监控下次 yield
+        if request.callback is not None:
+            if inspect.isgeneratorfunction(request.callback):  # 很好用的库，判断是否是 yield 函数
+                for task in request.callback(request, response):
+                    if isinstance(task, Request):
+                        self.add_new_request(new_request=task, old_request=request, response=response)
+            else:
+                request.callback(request, response)
---
+++ spider/spider_jump.py
@@ -0,0 +1,150 @@
+"""
+    jump spider（本地执行，用于处理比较频繁的循环事件，完全独立正常的 spider）
+
+    jump spider 内置 spider_start、spider_close
+    jump spider 参数 request_middleware 可传请求中间件
+
+    注意：
+        jump spider 不走 spider、request 中间件、不走 记录装饰器
+        jump spider 不能 yield item yield 的都不会处理
+        jump spider 最后会执行 jump_out 合并 cookie、meta，可以自定义
+"""
+import inspect
+from abc import abstractmethod
+from typing import Union, List, Callable
+from palp.network.request import Request
+from palp.network.response import Response
+from requests.cookies import RequestsCookieJar
+from palp.exception import NotGeneratorFunctionError
+from palp.sequence.sequence_memory import PriorityMemorySequence
+from palp.controller.controller_spider_jump import JumpController
+
+
+class JumpSpider:
+    """
+        jump spider（用于处理比较频繁的循环事件）
+
+        注意：不走 spider 中间件，不处理 item
+    """
+
+    def __init__(self, request: Request, request_middleware: Union[List[Callable], Callable] = None, **kwargs):
+        """
+
+        :param request: jump 之后需要执行的请求
+        :param request_middleware: 请求中间件
+        :param kwargs: 需要被 self.xxx 访问到的参数
+        """
+        self.queue = PriorityMemorySequence()  # 请求队列
+        self.request = request
+        self.request_middleware = request_middleware
+
+        # 导入一下自定义设置
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def spider_start(self) -> None:
+        """
+        spider 开始时的操作
+
+        :param spider:
+        :return:
+        """
+
+    def spider_close(self) -> None:
+        """
+        spider 开始时的操作
+
+        :param spider:
+        :return:
+        """
+
+    @abstractmethod
+    def start_requests(self) -> None:
+        """
+        起始函数
+
+        :return:
+        """
+        pass
+
+    def start_distribute(self) -> None:
+        """
+        刚开始的分发任务
+
+        :return:
+        """
+        # 校验是否是生成器函数
+        if not inspect.isgeneratorfunction(self.start_requests):
+            raise NotGeneratorFunctionError("start_requests 必须 yield Request!")
+
+        # 检查起始函数发出请求
+        for request in self.start_requests():
+            if not isinstance(request, Request):
+                raise ValueError("start_requests 仅支持 yield Request")
+
+            # 起始函数无 callback 默认添加
+            if request.callback is None:
+                request.callback = self.parse
+
+            # 为每一个起始函数添加一个 cookie_jar，如果有就不加
+            if not request.cookie_jar:
+                request.cookie_jar = RequestsCookieJar()
+
+            self.queue.put(request)
+
+    def parse(self, request: Request, response: Response) -> None:
+        """
+        默认的解析函数（仅 start_requests 默认的）
+
+        @result:
+        """
+
+    def run(self) -> None:
+        """
+        执行 jump spider
+
+        :return:
+        """
+        self.spider_start()
+
+        try:
+            # 分发任务
+            self.start_distribute()
+
+            # 执行流程
+            JumpController(q=self.queue, request_middleware=self.request_middleware, spider=self).run()
+        finally:
+            self.spider_close()
+
+    def jump_out(self, request: Request):
+        """
+        jump spider 执行结束之后，将会调用合并 cookie、meta
+
+        :param request:
+        :return:
+        """
+        self.request.cookie_jar.update(request.cookie_jar)
+        if self.request.meta:
+            self.request.meta.update(request.meta)
+
+    def __setattr__(self, key, value):
+        """
+        设置属性
+
+        :param key:
+        :param value:
+        :return:
+        """
+        self.__dict__[key] = value
+
+    def __getattr__(self, key):
+        """
+        获取属性
+
+        :param key:
+        :return:
+        """
+        if key not in self.__dict__:
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{key}'")
+
+        return self.__dict__[key]
